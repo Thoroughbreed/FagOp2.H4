591b7472be7e1246578efb37059a527b5ae7ad30
Jan Andreasen <jan@tved.it>
2022-02-16 13:25:26 +0100

    Fucking auto != auth

--- WebAPI/Program.cs
+++ WebAPI/Program.cs
@@ -1,8 +1,14 @@
+using System;
 using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.IdentityModel.Tokens;
 using WebAPI.DAL;
 using WebAPI.DTO;
 using WebAPI.Models;
@@ -14,6 +20,23 @@ builder.Services.AddSwaggerGen();
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 builder.Services.AddCors();
 builder.Services.AddDbContext<TodoDb>(opt => opt.UseInMemoryDatabase("TodoList"));
+string domain = $"https://{builder.Configuration["Auth0:Domain"]}/";
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(options =>
+    {
+        options.Authority = domain;
+        options.Audience = builder.Configuration["Auto0:Audience"];
+        options.TokenValidationParameters = new TokenValidationParameters
+        {
+            NameClaimType = ClaimTypes.NameIdentifier
+        };
+    });
+builder.Services.AddAuthorization(options =>
+{
+    options.AddPolicy("read:messages",
+        policy => policy.Requirements.Add(new HasScopeRequirement("read:messages", domain)));
+});
+builder.Services.AddSingleton<IAuthorizationHandler, HasScopeHandler>();
 
 var app = builder.Build();
 
@@ -23,76 +46,111 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
-app.UseCors(o => {
+app.UseCors(o =>
+{
     o.AllowAnyOrigin();
     o.AllowAnyHeader();
     o.AllowAnyMethod();
 });
+
+app.UseAuthentication();
+app.UseAuthorization();
 
 app.MapGet("/", () => "Hello World!");
 
 #region API Endpoints
 
 // GET - default, all non-completed
-    app.MapGet("/todoitems", async (TodoDb db) =>
-    {
-        var items = await db.Todos.Select(x => new TodoItemDTO(x)).ToListAsync();
-        return items.Where(x => x.Completed == false);
-    });
+app.MapGet("/todoitems", async (TodoDb db) =>
+{
+    var items = await db.Todos.Select(x => new TodoItemDTO(x)).ToListAsync();
+    return items.Where(x => x.Completed == false);
+}).RequireAuthorization();
 
 // GET - all items
-    app.MapGet("/todoitems/all", async (TodoDb db) =>
-        await db.Todos.Select(x => new TodoItemDTO(x)).ToListAsync());
+app.MapGet("/todoitems/all", async (TodoDb db) =>
+    await db.Todos.Select(x => new TodoItemDTO(x)).ToListAsync()).RequireAuthorization();
 
 // GET - item by ID
-    app.MapGet("/todoitems/{id}", async (int id, TodoDb db) =>
-        await db.Todos.FindAsync(id) is Todo todo ? Ok(new TodoItemDTO(todo)) : NotFound());
+app.MapGet("/todoitems/{id}", async (int id, TodoDb db) =>
+    await db.Todos.FindAsync(id) is Todo todo ? Ok(new TodoItemDTO(todo)) : NotFound()).RequireAuthorization();
 
 // POST - new item
-    app.MapPost("/todoitems", async (TodoItemDTO todoItemDTO, TodoDb db) =>
+app.MapPost("/todoitems", async (TodoItemDTO todoItemDTO, TodoDb db) =>
+{
+    var todoItem = new Todo
     {
-        var todoItem = new Todo
-        {
-            Completed = todoItemDTO.Completed,
-            Description = todoItemDTO.Description,
-            Priority = todoItemDTO.Priority
-        };
+        Completed = todoItemDTO.Completed,
+        Description = todoItemDTO.Description,
+        Priority = todoItemDTO.Priority
+    };
 
-        db.Todos.Add(todoItem);
-        await db.SaveChangesAsync();
+    db.Todos.Add(todoItem);
+    await db.SaveChangesAsync();
 
-        return Created($"/todoitems/{todoItem.Id}", new TodoItemDTO(todoItem));
-    });
+    return Created($"/todoitems/{todoItem.Id}", new TodoItemDTO(todoItem));
+}).RequireAuthorization();
 
 // PUT - edit item by ID
-    app.MapPut("/todoitems/{id}", async (int id, TodoItemDTO todoItemDTO, TodoDb db) =>
-    {
-        var todo = await db.Todos.FindAsync(id);
+app.MapPut("/todoitems/{id}", async (int id, TodoItemDTO todoItemDTO, TodoDb db) =>
+{
+    var todo = await db.Todos.FindAsync(id);
 
-        if (todo == null) return NotFound();
+    if (todo == null) return NotFound();
 
-        todo.Description = todoItemDTO.Description;
-        todo.Completed = todoItemDTO.Completed;
-        todo.Priority = todoItemDTO.Priority;
+    todo.Description = todoItemDTO.Description;
+    todo.Completed = todoItemDTO.Completed;
+    todo.Priority = todoItemDTO.Priority;
 
-        await db.SaveChangesAsync();
+    await db.SaveChangesAsync();
 
-        return NoContent();
-    });
+    return NoContent();
+}).RequireAuthorization();
 
 // DELETE - remove item by ID
-    app.MapDelete("/todoitems/{id}", async (int id, TodoDb db) =>
+app.MapDelete("/todoitems/{id}", async (int id, TodoDb db) =>
+{
+    if (await db.Todos.FindAsync(id) is Todo todo)
     {
-        if (await db.Todos.FindAsync(id) is Todo todo)
-        {
-            db.Todos.Remove(todo);
-            await db.SaveChangesAsync();
-            return Ok();
-        }
+        db.Todos.Remove(todo);
+        await db.SaveChangesAsync();
+        return Ok();
+    }
 
-        return NoContent();
-    });
+    return NoContent();
+}).RequireAuthorization();
 
 #endregion
 
 app.Run();
+
+public class HasScopeRequirement : IAuthorizationRequirement
+{
+    public string Issuer { get; }
+    public string Scope { get; }
+
+    public HasScopeRequirement(string scope, string issuer)
+    {
+        Scope = scope ?? throw new ArgumentNullException(nameof(scope));
+        Issuer = issuer ?? throw new ArgumentNullException(nameof(issuer));
+    }
+}
+
+public class HasScopeHandler : AuthorizationHandler<HasScopeRequirement>
+{
+    protected override Task HandleRequirementAsync(AuthorizationHandlerContext context, HasScopeRequirement requirement)
+    {
+        // If user does not have the scope claim, get out of here
+        if (!context.User.HasClaim(c => c.Type == "scope" && c.Issuer == requirement.Issuer))
+            return Task.CompletedTask;
+
+        // Split the scopes string into an array
+        var scopes = context.User.FindFirst(c => c.Type == "scope" && c.Issuer == requirement.Issuer).Value.Split(' ');
+
+        // Succeed if the scope array contains the required scope
+        if (scopes.Any(s => s == requirement.Scope))
+            context.Succeed(requirement);
+
+        return Task.CompletedTask;
+    }
+}
